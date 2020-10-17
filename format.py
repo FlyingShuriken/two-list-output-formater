79ca42a8ee73e11c3a722b3bcbdd30ad5f7ca500
FlyingShuriken <59129640+FlyingShuriken@users.noreply.github.com>
2020-10-17 23:04:16 +0800

    Update format.py
    sorry for my english

--- format.py
+++ format.py
@@ -1,16 +1,18 @@
 def fmt(a):
-  first = len(tri_length)-1
-  b = len(str(tri_length[a]))
-  for x in range(len(tri_length)):
-      nowlen = len(str(tri_length[x]))
-      diflen = a - nowlen
-      nowstring = tri_length[x]
-      s = ''
-      for y in range(diflen+5):
-          s += ' '
-      newstring = f"{nowstring}{s}"
-      tri_length[x] = newstring
+  f = len(a)-1                                                      #get the first elemen's index
+  m = len(str(a[f]))                                                #get the first elemen
+  for x in range(len(a)):                                           #process in every elemen
+      nowlen = len(str(a[x]))                                       #the length of string now processing
+      diflen = f - nowlen                                           #calculate the spaces that needed
+      nowstring = a[x]                                              #get the elemen in string that are processing now
+      s = ''                                                        #set an empty string as 's'
+      for y in range(diflen+5):                                     #adding spaces into s, 5 is a random number for looking better in the output
+          s += ' '                                                  #start to add space into s
+      newstring = f"{nowstring}{s}"                                 #using f string instead of + in string
+      a[x] = newstring                                              #replace the int to the formated string
+  return a                                                          #return the formated list
 
-print("------------------------------------")
-res = "\n".join("{} {}".format(x, y) for x, y in zip(tri_length, tri_surface))
-print(res)
+if __name__ == "__main__":
+  a = fmt(a)
+  res = "\n".join("{} {}".format(x, y) for x, y in zip(a,b))        #print two list in same line
+  print(res)
